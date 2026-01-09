325309fb6ecd2f7c1c722c69d788d5884f83eadd
Petr Kozachenko <pkozachenko@alfabank.ru>
2026-01-09 16:36:47 +0300

    vercel

---
+++ api/bot.js
@@ -0,0 +1,31 @@
+import TelegramBot from "node-telegram-bot-api";
+
+const bot = new TelegramBot(process.env.BOT_TOKEN);
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(200).send("OK");
+  }
+
+  const update = req.body;
+
+  if (update.message?.text === "/start") {
+    await bot.sendMessage(
+      update.message.chat.id,
+      "Открой Mini App 👇",
+      {
+        reply_markup: {
+          keyboard: [[{
+            text: "Открыть приложение",
+            web_app: {
+              url: "https://delphin-orpin.vercel.app/index.html"
+            }
+          }]],
+        resize_keyboard: true
+        }
+      }
+    );
+  }
+
+  res.status(200).send("OK");
+}
